189e316f28efcf51947c3c071124554b3cd85c24
LeonamOliveira <oliveira.leonam@hotmail.com>
2018-03-29 17:32:33 -0300

    Módulo 8

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -1,14 +1,14 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-const creditShema = new mongoose.Schema({
+const creditSchema = new mongoose.Schema({
     name: { type: String, required: true },
     value: { type: Number, min: 0, required: true }
 })
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
     status: { type: String, required: false, uppercase: true,
         enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
 })
@@ -17,7 +17,7 @@ const billingCycleSchema = new mongoose.Schema({
     name: { type: String, required: true },
     month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
-    credits: [ creditSchema ],
+    credits: [creditSchema],
     debts: [ debtSchema ]
 })
 
