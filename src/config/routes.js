bca6d22201be8c95dd1c5306e60d8d4d411226b9
LeonamOliveira <l.oliveira@adaptersistemas.com.br>
2018-05-15 17:20:35 -0300

    Aula 148

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,12 +1,30 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
 
-    //Definir URL base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router)
+    /*
+    * Rotas protegidas por Token JWT
+    */
 
-    //Rotas de Ciclo do pagamento
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    /*
+    * Rotas Abertas
+    */
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
 }
